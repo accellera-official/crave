6daeac0343472c4d8f0b0e3ce23c975d6d723821
Thilo Vörtler <thilo.voertler@coseda-tech.com>
2023-04-05 15:18:33 +0200

    Coseda Copyright added

--- crave/src/crave/experimental/Object.hpp
+++ crave/src/crave/experimental/Object.hpp
@@ -4,6 +4,7 @@
 //	Copyright (c) 2012-2020 University of Bremen, Germany. 
 //  	Copyright (c) 2015-2020 DFKI GmbH Bremen, Germany.
 //  	Copyright (c) 2020 Johannes Kepler University Linz, Austria.
+//    Copyright (c) 2022 - 2023 Coseda Technologies GmbH.
 //
 //	Permission is hereby granted, free of charge, to any person obtaining a copy
 //	of this software and associated documentation files (the "Software"), to deal
